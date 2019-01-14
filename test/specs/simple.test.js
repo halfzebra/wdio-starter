7e68b06690b0b13aabcc1c0d463c492ceef5f964
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2019-01-14 13:40:35 +0100

    Added .editorconfig and applied formatting

--- test/specs/simple.test.js
+++ test/specs/simple.test.js
@@ -1,9 +1,12 @@
-const assert = require('assert');
+const assert = require("assert");
 
-describe('webdriver.io page', () => {
-    it('should have the right title', () => {
-        browser.url('https://webdriver.io');
-        const title = browser.getTitle();
-        assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
-    });
+describe("webdriver.io page", () => {
+  it("should have the right title", () => {
+    browser.url("https://webdriver.io");
+    const title = browser.getTitle();
+    assert.equal(
+      title,
+      "WebdriverIO · Next-gen WebDriver test framework for Node.js"
+    );
+  });
 });
